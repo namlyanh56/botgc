f75d10ff8a3b76d16c26e7d5274928706eaa4999
Arif Muhasyir <arthurash911@gmail.com>
2025-12-07 00:22:04 +0700

    Update menu.js

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -15,30 +15,29 @@ const MENU = {
 
 // Tampilkan tombol sesuai status login
 function mainMenu(ctx) {
-  const acc = ctx ?  getAcc(ctx.from.id) : null;
-  const loggedIn = ! !(acc && acc. authed);
-  
+  const acc = ctx ? getAcc(ctx.from.id) : null;
+  const loggedIn = !!(acc && acc.authed);
+
   const kb = new Keyboard();
-  
+
   if (loggedIn) {
     // Menu untuk user yang sudah login
     kb
-      .text(MENU.createFromList). text(MENU.createSequential). row()
+      .text(MENU.createFromList).text(MENU.createSequential).row()
       .text(MENU.huntUsername).text(MENU.stopHunt).row()
       .text(MENU.lastResult).text(MENU.help).row()
       .text(MENU.logout).row();
   } else {
     // Menu untuk user yang belum login
-    kb
-      .text(MENU.login).text(MENU.help).row();
+    kb.text(MENU.login).text(MENU.help).row();
   }
-  
-  return kb. resized();
+
+  return kb.resized();
 }
 
 // Inline tombol BATAL untuk kembali ke menu awal
 function inlineCancelKb() {
-  return new InlineKeyboard(). text('❌ Batal', 'action:cancel');
+  return new InlineKeyboard().text('❌ Batal', 'action:cancel');
 }
 
 module.exports = { MENU, mainMenu, inlineCancelKb };
